7a22957557fc4cf72a1ebcf957775c4f2cf93a65
chutchaikp <chutchai.kp@gmail.com>
2023-06-07 02:02:49 +0700

    Add console.log

--- Hello.sol
+++ Hello.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.18;
+
+import "hardhat/console.sol";
 
 contract Hello {
     string message;
 
     function setMessage(string memory newMessage) public {
+        console.log("set message");
         message = newMessage;
     }
 
     function getMessage() public view returns (string memory message_) {
+        console.log("get message now", msg.sender);
         return message;
     }
 }
