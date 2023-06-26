75f1e9cc94ab495155774c3c0a035ff66ce44de7
chutchaikp <chutchai.kp@gmail.com>
2023-06-26 08:47:44 +0700

    commit

--- freeCodeCamp2/FundMe.sol
+++ freeCodeCamp2/FundMe.sol
@@ -1,16 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 // Smart contracts can hold funds just like how wallets can
 
-contract FundMe {
+// Extension Method
+import "./PriceConverter.sol";
 
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public  funders;
+    mapping(address => uint256) public addressToAmountFunded;
     
     function fund() public payable  {
         // Want to be able to set a minimum fund amoun
         // 1. How do we send ETH to this contract ?
-        require(msg.value > 1e18, "Didn't send enough!");
+        // require(msg.value > 1e18, "Didn't send enough!");
         
+        uint256 rate = msg.value.getConversionRate();
+        require(rate > minimumUsd, "Didn't send enough!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    // ABI 
+    // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+
+    function withdraw() public {
+
+        for (uint256 i = 0; i < 10; i++ ) 
+        {
+            
+        }
     }
 
 }
--- tech.md
+++ tech.md
@@ -3,3 +3,7 @@
 - select similar text in a file 
 1. select some text
 2. cmd + d 
+
+
+> Youtube: Learn Blockchain, Solidity, and Full Stack Web3 Development with JavaScript – 32-Hour Course
+4:38:00/1:07:54:30
